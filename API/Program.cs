715dde9f1b424367af417f22e93c439ae7bd35b9
Korir <sammy.korir@bell.corp.bce.ca>
2020-02-25 12:03:39 -0400

    Added semantic UI and Axios

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
@@ -16,26 +17,29 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            var host=CreateHostBuilder(args).Build();
-            using(var scope=host.Services.CreateScope()){
-                var services=scope.ServiceProvider;
-                try{
+            var host = CreateWebHostBuilder(args).Build();
+            
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try 
+                
+                {
                     var context = services.GetRequiredService<DataContext>();
                     context.Database.Migrate();
-                }catch(Exception ex){
-                    var logger= services.GetRequiredService<ILogger<Program>>();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
                     logger.LogError(ex, "An error occured during migration");
                 }
             }
-            
+
             host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
